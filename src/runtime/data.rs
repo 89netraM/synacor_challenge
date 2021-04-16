4cd9c7a63937107c3c23e60131d50a4de062080d
Mårten Åsberg <marten.asberg@outlook.com>
2021-04-16 14:03:33 +0200

    Tests for Data

--- src/runtime/data.rs
+++ src/runtime/data.rs
@@ -74,3 +74,145 @@ impl<'a> Data<'a> {
 		self.memory.len()
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	const MEMORY: &'static [u16] = &[21, 19, 77, 0, 32768];
+
+	#[test]
+	fn get_number() {
+		let data = Data::new(MEMORY);
+		assert_eq!(
+			data.get_number(2),
+			Ok(MEMORY[2]),
+			"Reading a valid number at a valid position."
+		);
+	}
+
+	#[test]
+	fn get_register() {
+		let data = Data::new(MEMORY);
+		assert_eq!(data.get_number(4), Ok(0), "Reading a register address.");
+	}
+
+	#[test]
+	fn get_invalid() {
+		let data = Data::new(MEMORY);
+		assert_eq!(
+			data.get_number(5),
+			Err("Reading from out of range address 5!".to_string()),
+			"Reading a register address."
+		);
+	}
+
+	#[test]
+	fn update_register() {
+		const TEST_VALUE: u16 = 42;
+		let mut data = Data::new(MEMORY);
+		data.set_number(4, TEST_VALUE).unwrap();
+		assert_eq!(
+			data.get_number(4),
+			Ok(TEST_VALUE),
+			"Updating the value of a register."
+		);
+	}
+
+	#[test]
+	fn update_non_register() {
+		let mut data = Data::new(MEMORY);
+		let result = data.set_number(2, 42);
+		assert_eq!(
+			result,
+			Err(format!(
+				"Number at {} ({}) is not a register!",
+				2, MEMORY[2]
+			)),
+			"Updating the value of a address that's not a register."
+		);
+	}
+
+	#[test]
+	fn update_invalid() {
+		let mut data = Data::new(MEMORY);
+		let result = data.set_number(5, 42);
+		assert_eq!(
+			result,
+			Err("Reading from out of range address 5!".to_string()),
+			"Updating to an address out of range."
+		);
+	}
+
+	#[test]
+	fn push_and_pop() {
+		const TEST_VALUE: u16 = 42;
+		let mut data = Data::new(MEMORY);
+		data.push_stack(TEST_VALUE);
+		assert_eq!(
+			data.pop_stack(),
+			Ok(TEST_VALUE),
+			"Push and then pop the same value."
+		);
+	}
+
+	#[test]
+	fn only_pop() {
+		let mut data = Data::new(MEMORY);
+		let result = data.pop_stack();
+		assert_eq!(
+			result,
+			Err("Stack was empty when popping!".to_string()),
+			"Poping when the stack is empty."
+		);
+	}
+
+	#[test]
+	fn reading_memory() {
+		let data = Data::new(MEMORY);
+		assert_eq!(
+			data.read_memory(2),
+			Ok(MEMORY[2]),
+			"Reading from unchanged memory."
+		);
+	}
+
+	#[test]
+	fn reading_invalid_memory() {
+		let data = Data::new(MEMORY);
+		assert_eq!(
+			data.read_memory(5),
+			Err("Reading from out of range address 5!".to_string()),
+			"Reading from out of range memory."
+		);
+	}
+
+	#[test]
+	fn writing_and_reading_memory() {
+		const TEST_VALUE: u16 = 42;
+		let mut data = Data::new(MEMORY);
+		data.write_memory(2, TEST_VALUE).unwrap();
+		assert_eq!(
+			data.read_memory(2),
+			Ok(TEST_VALUE),
+			"Reading from changed memory."
+		);
+	}
+
+	#[test]
+	fn writing_invalid_memory() {
+		let mut data = Data::new(MEMORY);
+		let result = data.write_memory(5, 42);
+		assert_eq!(
+			result,
+			Err("Writing to out of range address 5!".to_string()),
+			"Writing to memory out of range."
+		);
+	}
+
+	#[test]
+	fn length_memory() {
+		let data = Data::new(MEMORY);
+		assert_eq!(data.length_memory(), MEMORY.len(), "Memory length.");
+	}
+}
